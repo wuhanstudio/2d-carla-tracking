8694d98f2cc0a47ff3ce8a03707204e39871b234
wuhanstudio <wuhanstudio@qq.com>
2024-05-21 21:42:17 +0100

    [tracking] Add YOLO OC-SORT

---
+++ 2d-tracking-yolo-oc-sort.py
@@ -0,0 +1,188 @@
+import carla
+
+import os
+import queue
+import random
+
+import cv2
+import numpy as np
+
+from oc_sort.ocsort import OCSort
+
+from utils.box_utils import draw_bounding_boxes
+from utils.projection import *
+from utils.world import *
+
+## Part 0: Object Detection model
+
+from what.models.detection.datasets.coco import COCO_CLASS_NAMES
+from what.models.detection.yolo.yolov4 import YOLOV4
+from what.models.detection.yolo.yolov4_tiny import YOLOV4_TINY
+
+from what.cli.model import *
+from what.utils.file import get_file
+
+# Check what_model_list for all supported models
+what_yolov4_model_list = what_model_list[4:6]
+
+index = 0 # YOLOv4
+# index = 1 # YOLOv4 Tiny
+
+# Download the model first if not exists
+WHAT_YOLOV4_MODEL_FILE = what_yolov4_model_list[index][WHAT_MODEL_FILE_INDEX]
+WHAT_YOLOV4_MODEL_URL  = what_yolov4_model_list[index][WHAT_MODEL_URL_INDEX]
+WHAT_YOLOV4_MODEL_HASH = what_yolov4_model_list[index][WHAT_MODEL_HASH_INDEX]
+
+if not os.path.isfile(os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE)):
+    get_file(WHAT_YOLOV4_MODEL_FILE,
+             WHAT_MODEL_PATH,
+             WHAT_YOLOV4_MODEL_URL,
+             WHAT_YOLOV4_MODEL_HASH)
+
+# Darknet
+model = YOLOV4(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE))
+# model = YOLOV4_TINY(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE))
+
+def camera_callback(image, rgb_image_queue):
+    rgb_image_queue.put(np.reshape(np.copy(image.raw_data),
+                        (image.height, image.width, 4)))
+
+# Part 1
+client = carla.Client('localhost', 2000)
+world = client.get_world()
+
+# Set up the simulator in synchronous mode
+settings = world.get_settings()
+settings.synchronous_mode = True  # Enables synchronous mode
+settings.fixed_delta_seconds = 0.05
+world.apply_settings(settings)
+
+# Get the world spectator
+spectator = world.get_spectator()
+
+# Get the map spawn points
+spawn_points = world.get_map().get_spawn_points()
+
+# Spawn the ego vehicle
+bp_lib = world.get_blueprint_library()
+vehicle_bp = bp_lib.find('vehicle.lincoln.mkz_2020')
+vehicle = world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
+
+# Spawn the camera
+camera_bp = bp_lib.find('sensor.camera.rgb')
+# [Windows Only] Fixes https://github.com/carla-simulator/carla/issues/6085
+camera_bp.set_attribute('image_size_x', '640')
+camera_bp.set_attribute('image_size_y', '640')
+
+camera_init_trans = carla.Transform(carla.Location(x=1, z=2))
+camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=vehicle)
+
+# Create a queue to store and retrieve the sensor data
+image_queue = queue.Queue()
+camera.listen(lambda image: camera_callback(image, image_queue))
+
+# Clear existing NPCs
+clear_npc(world)
+clear_static_vehicle(world)
+
+# Part 2
+
+# Remember the edge pairs
+edges = [[0, 1], [1, 3], [3, 2], [2, 0], [0, 4], [4, 5],
+         [5, 1], [5, 7], [7, 6], [6, 4], [6, 2], [7, 3]]
+
+# Get the world to camera matrix
+world_2_camera = np.array(camera.get_transform().get_inverse_matrix())
+
+# Get the attributes from the camera
+image_w = camera_bp.get_attribute("image_size_x").as_int()
+image_h = camera_bp.get_attribute("image_size_y").as_int()
+fov = camera_bp.get_attribute("fov").as_float()
+
+# Calculate the camera projection matrix to project from 3D -> 2D
+K = build_projection_matrix(image_w, image_h, fov)
+K_b = build_projection_matrix(image_w, image_h, fov, is_behind_camera=True)
+
+# Spawn NPCs
+for i in range(50):
+    vehicle_bp = bp_lib.filter('vehicle')
+
+    # Exclude bicycle
+    car_bp = [bp for bp in vehicle_bp if int(
+        bp.get_attribute('number_of_wheels')) == 4]
+    npc = world.try_spawn_actor(random.choice(
+        car_bp), random.choice(spawn_points))
+
+    if npc:
+        npc.set_autopilot(True)
+
+vehicle.set_autopilot(True)
+
+mot_tracker = OCSort(det_thresh=0.6, iou_threshold=0.3, use_byte=False)
+
+# Main loop
+while True:
+    try:
+        world.tick()
+
+        # Move the spectator to the top of the vehicle 
+        transform = carla.Transform(vehicle.get_transform().transform(carla.Location(x=-4,z=50)), carla.Rotation(yaw=-180, pitch=-90)) 
+        spectator.set_transform(transform) 
+
+        # Display RGB camera image
+        origin_image = image_queue.get()
+
+        # Image preprocessing
+        image = cv2.cvtColor(origin_image, cv2.COLOR_BGR2RGB)
+
+        # Run inference
+        images, boxes, labels, probs = model.predict(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Only draw 2: car, 5: bus, 7: truck
+        boxes = np.array([box for box, label in zip(boxes, labels) if label in [2, 5, 7]])
+        probs = np.array([prob for prob, label in zip(probs, labels) if label in [2, 5, 7]])
+        labels = np.array([2 for label in labels if label in [2, 5, 7]])
+
+        # convert [x1, y1, w, h] to [x1, y1, x2, y2]
+        if len(boxes) > 0:
+            sort_boxes = boxes.copy()
+
+            # (xc, yc, w, h) --> (x1, y1, x2, y2)
+            height, width, _ = image.shape
+
+            for box in sort_boxes:
+                box[0] *= width
+                box[1] *= height
+                box[2] *= width 
+                box[3] *= height
+
+                # From center to top left
+                box[0] -= box[2] / 2
+                box[1] -= box[3] / 2
+
+                # From width and height to x2 and y2
+                box[2] += box[0]
+                box[3] += box[1]
+
+            dets = np.concatenate((np.array(sort_boxes), np.array(probs).reshape((len(probs), -1))), axis=1)
+
+            # Update tracker
+            trackers = mot_tracker.update(dets, [height, width], (height, width))
+
+            # Draw bounding boxes onto the image
+            output = draw_bounding_boxes(
+                image, trackers[:, 0:4], labels, COCO_CLASS_NAMES, trackers[:, 4])
+
+        cv2.imshow('2D YOLOv4 OC-SORT', image)
+
+        # Quit if user presses 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    except KeyboardInterrupt as e:
+        break
+
+clear(world, camera)
+cv2.destroyAllWindows()
